bc0590402887a89bca8ccf7e89fd59a60c698c88
Marina <zvyagintsevame@yandex.ru>
2021-05-27 07:35:39 +0200

    complete task 1-3 from module 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,22 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: "December",
+  currency: "USD",
+  isPayed: false,
+};
+
+function sumSalary(obj) {
+  let sumSalary = 0;
+
+  for (let key in obj) {
+    if (isFinite(obj[key])) {
+      sumSalary += obj[key];
+    }
+  }
+
+  return sumSalary;
 }
+
+sumSalary(salaries);
