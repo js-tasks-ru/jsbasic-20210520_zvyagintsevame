2a1dbbcecc004e2e4d9a8352746d3a9b6973f139
Marina <zvyagintsevame@yandex.ru>
2021-06-09 07:22:17 +0200

    complete task 2 from module 4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,6 @@
+const table = document.body.firstElementChild;
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let i = 0; i < 5; i++) {
+    table.rows[i].cells[i].style.background = 'red'
+  }
 }
