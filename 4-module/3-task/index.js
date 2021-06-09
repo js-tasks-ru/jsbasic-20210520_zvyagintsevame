f395651c41fa75f5f58f248863908d200d71dc3d
Marina <zvyagintsevame@yandex.ru>
2021-06-09 09:48:06 +0200

    complete task 3 from module 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,45 @@
+const statusIndex = 3;
+const genderIndex = 2;
+const ageIndex = 1;
+
 function highlight(table) {
-  // ваш код...
+
+  for (let tBody of table.tBodies) {
+    const rows = tBody.rows;
+
+    for (row of rows) {
+      const tdStatusElement = row.cells[statusIndex];
+      const available = tdStatusElement.dataset.available;
+
+      if (available === 'true') {
+        row.classList.add('available');
+      }
+      else if (available === 'false') {
+        row.classList.add('unavailable');
+      }
+      else if (!available)
+        row.setAttribute('hidden', 'hidden');
+    }
+
+    for (row of rows) {
+      const tdGenderElement = row.cells[genderIndex];
+      const gender = tdGenderElement.textContent;
+
+      if (gender === 'm') {
+        row.classList.add('male')
+      }
+      else {
+        row.classList.add('female')
+      }
+    }
+
+    for (row of rows) {
+      const tdAgeElement = row.cells[ageIndex];
+      const age = tdAgeElement.textContent
+
+      if (+age < 18) {
+        row.style.textDecoration = 'line-through';
+      }
+    }
+  }
 }
