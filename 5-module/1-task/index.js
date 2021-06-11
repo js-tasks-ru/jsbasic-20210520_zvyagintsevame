d657f09f86e821860489e4c95d57883af09ae429
Marina <zvyagintsevame@yandex.ru>
2021-06-11 14:25:25 +0200

    complete task 1 from module 5

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,6 +1,5 @@
-const button = document.querySelector('.hide-self-button');
-
 function hideSelf() {
+  const button = document.querySelector('.hide-self-button');
   button.addEventListener("click", () => {
     button.hidden = true;
   })
